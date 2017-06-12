ac0f019d2499401ebe58ab0ea4a237ebc5cc0cf4
TheOpti <wieckowski.piotr.93@gmail.com>
2017-06-12 16:56:04 +0200

    Mock sections for future

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,25 @@ var cleanCSS = require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
 
 gulp.task('browser-sync', ['sass', 'minify-css'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
-gulp.task('sass', function () {
-    return gulp.src('scss/*.scss')
-      .pipe(sass())
-      .pipe(gulp.dest('css'))
-      .pipe(browserSync.reload({stream: true}));
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass', 'minify-css']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch("scss/*.scss", ['sass', 'minify-css']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('minify-css', ['sass'], function() {
