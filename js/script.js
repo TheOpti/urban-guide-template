6c0ce1c2884c40e30e8abec93cb759a50372a6eb
TheOpti <wieckowski.piotr.93@gmail.com>
2017-06-21 09:43:56 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-
 const NAVBAR_HEIGHT = 60;
 
 $(document).ready(() => {
-  let navBar = $('#nav-bar');
+  const navBar = $('#nav-bar');
   navBar.addClass("navigation--theme-default");
-
-  changeNavBar();
 
   $(window).on('scroll', (event) => {
     changeNavBar();
   });
 
+  $(window).on('load', () => {
+    createMap();
+  });
+
   navBar.on('click', (event) => {
     if ($(event.target).hasClass('navigation__menu-item')) {
-      const elemOffset = $( `#${event.target.dataset.section}` ).offset().top;
+      const elemOffset = $(`#${event.target.dataset.section}`).offset().top;
 
       $('html, body').animate({
-          scrollTop: elemOffset - NAVBAR_HEIGHT
+        scrollTop: elemOffset - NAVBAR_HEIGHT
       }, 750);
     }
   });
@@ -30,4 +31,26 @@ $(document).ready(() => {
       navBar.addClass('navigation--theme-default');
     }
   }
+
+  function createMap() {
+    var mapProp = {
+      center: new google.maps.LatLng(51.508742, -0.120850),
+      zoom: 15,
+      clickableIcons: true,
+      disableDefaultUI: true,
+      scrollwheel: false
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var marker = new google.maps.Marker({
+      position: mapProp.center
+    });
+
+    marker.setMap(map);
+  }
+
+
+  changeNavBar();
+
 });
