cf531732d544ec3903c90723a69072b53ace7eac
TheOpti <wieckowski.piotr.93@gmail.com>
2019-08-05 23:11:18 +0200

    Add responsiveness for header and navigation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ $(document).ready(() => {
   const navBar = $('#nav-bar');
   navBar.addClass("navigation--theme-default");
 
+  const menu = document.querySelector('.navigation__menu');
+  const shadow = document.querySelector('.navigation__shadow');
+  const hamburgerMenuBtn = document.querySelector('.navigation__menu-btn');
+
   navBar.on('click', (event) => {
     if ($(event.target).hasClass('navigation__menu-item')) {
       const elemOffset = $(`#${event.target.dataset.section}`).offset().top;
@@ -14,8 +18,21 @@ $(document).ready(() => {
     }
   });
 
+  hamburgerMenuBtn.addEventListener('click', openMenu);
+  shadow.addEventListener('click', closeMenu);
+
   $(window).on('scroll', changeNavBar);
   $(window).on('load', createMap);
+
+  function openMenu() {
+    menu.classList.toggle('navigation__menu--open');
+    shadow.classList.toggle('navigation__shadow--open');
+  }
+
+  function closeMenu() {
+    menu.classList.toggle('navigation__menu--open');
+    shadow.classList.toggle('navigation__shadow--open');
+  }
 
   function changeNavBar() {
     if (window.scrollY > 50) {
